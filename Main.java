2dc7449bc41cbc41cc389f78ae97c6e5b244596d
SeamusC5600 <seamusridesbikes@gmail.com>
2025-01-06 10:48:54 -0500

    added names

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+//NAME: Seamus Cullen and Zack Panagiotakopoulos
 import java.util.Scanner;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
     }
 }

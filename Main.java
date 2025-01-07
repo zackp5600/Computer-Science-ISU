62ebb10a11e389675f48b359f4aaea5c5c5deca2
SeamusC5600 <seamusridesbikes@gmail.com>
2025-01-07 10:11:16 -0500

    added for loops

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 //NAME: Seamus Cullen and Zack Panagiotakopoulos
 import java.util.Scanner;
-import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //SETUP STAGE
+        String[] teamNames = new String[7];
+        System.out.println("Input team names");
+        for(int i = 0; i < 8; i++) {
+            String x =sc.nextLine();
+            for(int j = 0; j < i; j++){
+                if(teamNames[j].equals(x)){
+                    System.out.println("This is a duplicate")
+                }
+            }
+            if()
+            teamNames[i] =x;
+            
+        }
         
+
+
     }
 }

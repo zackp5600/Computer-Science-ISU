7da7eb4adcf672ed7da8fc44ad59cca9f8b27e9c
Zack <101464605+zackp5600@users.noreply.github.com>
2025-01-08 15:43:11 +0000

    Co-authored-by: SeamusC5600 <SeamusC5600@users.noreply.github.com>

--- tournament planner/src/Main.java
+++ tournament planner/src/Main.java
@@ -1,25 +1,51 @@
 //NAME: Seamus Cullen and Zack Panagiotakopoulos
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //SETUP STAGE
-        String[] teamNames = new String[7];
+        // SETUP STAGE
+        String[] teamNames = new String[8];
         System.out.println("Input team names");
-        for(int i = 0; i < 8; i++) {
-            String x =sc.nextLine();
-            for(int j = 0; j < i; j++){
-                if(teamNames[j].equals(x)){
-                    System.out.println("This is a duplicate")
+        // recieve team names
+        for (int i = 0; i < 8; i++) {
+            String x = sc.nextLine();
+            // this nested for loop checks for duplicates
+            for (int j = 0; j < i; j++) {
+                if (teamNames[j].equals(x)) {
+                    // if a duplicate is found then the user has to input another team name instead
+                    System.out.println("This is a duplicate. Enter another team name");
+                    i--;
+                    break;
+
                 }
             }
-            
-            teamNames[i] =x;
-            
-        }
-        
+            teamNames[i] = x;
 
+        }
+
+        // print the list of all the teams
+        System.out.println("The teams entered were: ");
+        for (int i = 0; i < 8; i++) {
+            System.out.println(teamNames[i]);
+        }
+        //GROUP STAGE
+        // now we randolmy assign teams to two groups
+
+        String[] GroupA = new String[4];
+        String[] GroupB = new String[4];
 
     }
 }
+
+/*
+ * red
+ * blue
+ * orange
+ * black
+ * white
+ * yellow
+ * silver
+ * green
+ */

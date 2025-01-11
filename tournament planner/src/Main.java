6e9adfb03128401646d979ca6beebf2c12850fa6
Zack <101464605+zackp5600@users.noreply.github.com>
2025-01-11 06:03:46 +0000

    redid the inputing to be alot simpler, fixed the randomization for the teams and added a simulation for the groups

--- tournament planner/src/Main.java
+++ tournament planner/src/Main.java
@@ -1,91 +1,121 @@
-
-//NAME: Seamus Cullen and Zack Panagiotakopoulos
-import java.util.Scanner;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // SETUP STAGE
-        ArrayList<String> teamNames = new ArrayList<String>();
-        System.out.println("Input team names");
-        // recieve team names
-        for (int i = 0; i < 8; i++) {
-            String x = sc.nextLine();
-            // this nested for loop checks for duplicates
-            for (int j = 0; j < i; j++) {
-                if (teamNames.get(j).equals(x)) {
-                    // if a duplicate is found then the user has to input another team name instead
-                    System.out.println("This is a duplicate. Enter another team name");
-                    i--;
-                    break;
-                }
+
+        // collect input from user
+        ArrayList<String> teamNames = new ArrayList<>();
+        System.out.println("Input 8 unique team names:");
+
+        // get teams and check for duplicates
+        while (teamNames.size() < 8) {
+            String teamName = sc.nextLine();
+
+            if (teamNames.contains(teamName)) {
+                System.out.println("This is a duplicate. Enter another team name:");
+            } else {
+                teamNames.add(teamName);
             }
-            teamNames.add(x);
         }
-        // print the list of all the teams
-        System.out.println("The teams entered were: ");
-        for (int i = 0; i < 8; i++) {
-            System.out.println(teamNames.get(i));
+
+        // Print the teams
+        System.out.println("\nThe teams entered were:");
+        for (String team : teamNames) {
+            System.out.println(team);
         }
-        // GROUP STAGE
-        // now we randomly assign teams to two groups using array list and if a number
-        // is choosen remove it
-        ArrayList<Integer> number = new ArrayList<Integer>();
-        number.add(0);
-        number.add(1);
-        number.add(2);
-        number.add(3);
-        number.add(4);
-        number.add(5);
-        number.add(6);
-        number.add(7);
+
+        // Copy of teamNames for use in creating randomly generated groups
+        ArrayList<String> remainingTeams = new ArrayList<>(teamNames);
 
         String[] GroupA = new String[4];
         String[] GroupB = new String[4];
-        for (int i = 0; i < 4; i++) {
-            int rng = (int) (Math.random() * ((number.size() - 1) - 0 + 1) + 0);
-            GroupA[i] = teamNames.get(number.get(rng));
-            teamNames.remove(teamNames.get(number.get(rng)));
-            System.out.println(rng);
-            if (number.contains(rng)) {
-                number.remove(number.indexOf(rng));
-            }
 
+        // create an array for tracking the points each team has in each group
+        int[] GroupA_pts = new int[4];
+        int[] GroupB_pts = new int[4];
+
+        // Assign teams to GroupA
+        for (int i = 0; i < 4; i++) {
+            int rng = (int) (Math.random() * remainingTeams.size());
+            GroupA[i] = remainingTeams.remove(rng); // Remove team from remaingTeams after assigning to GroupA
         }
 
-        // Ts 💔 not working yet
+        // There is no need to use the rng because we have already randomly assigned 4
+        // teams
         for (int i = 0; i < 4; i++) {
-            int rng = (int) (Math.random() * ((number.size() - 1) - 0 + 1) + 0);
-            GroupB[i] = teamNames.get(number.get(rng));
-            teamNames.remove(teamNames.get(number.get(rng)));
-            System.out.println(rng);
-            if (number.contains(rng)) {
-                number.remove(number.indexOf(rng));
-            }
+            GroupB[i] = remainingTeams.remove(0); // Remove from the beginning and add to groupB
         }
 
-        // Ts
+        // Print the teams in GroupA and GroupB
+        System.out.println("\nGroup A:");
+        for (String team : GroupA) {
+            System.out.println(team);
+        }
+
+        System.out.println("\nGroup B:");
+        for (String team : GroupB) {
+            System.out.println(team);
+        }
+
+        // create a array of vallues for the possible match outcomes
+        int[] possible_points = { 0, 1, 3 };
+
+        // Now we simulate the matches with the groups
+        // GroupA simulation
+
+        //i dont know for sure if the simulation is working rn but its looks good so far
         
-        System.out.println("GroupA");
         for (int i = 0; i < GroupA.length; i++) {
-            System.out.println(GroupA[i]);
+            for (int j = 1; j < GroupA.length - i; j++) {
+                int rng = (int) (Math.random() * 3);
+
+                // if loss than other team gets 3 points
+                if (rng == 0) {
+                    GroupA_pts[j] += possible_points[rng];
+                }
+                // if tie both teams get 1 point
+                else if (rng == 1) {
+                    GroupA_pts[i] += possible_points[rng];
+                    GroupA_pts[j] += possible_points[rng];
+                }
+                // if win this team gets 3 points and the other gets 0
+                else {
+                    GroupA_pts[i] += possible_points[rng];
+                }
+            }
         }
-        System.out.println("GroupB");
+
+        System.out.println("groupB");
+        // Group B simulation
         for (int i = 0; i < GroupB.length; i++) {
-            System.out.println(GroupB[i]);
+            for (int j = 1; j < GroupB.length - i; j++) {
+                int rng = (int) (Math.random() * 3);
+
+                // if loss than other team gets 3 points
+                if (rng == 0) {
+                    GroupB_pts[j] += possible_points[rng];
+                }
+                // if tie both teams get 1 point
+                else if (rng == 1) {
+                    GroupB_pts[i] += possible_points[rng];
+                    GroupB_pts[j] += possible_points[rng];
+                }
+                // if win this team gets 3 points and the other gets 0
+                else {
+                    GroupB_pts[i] += possible_points[rng];
+                }
+            }
+        }
+
+        System.out.println("Points");
+        for (int x : GroupA_pts) {
+            System.out.println(x);
+        }
+        System.out.println("Points");
+        for (int x : GroupB_pts) {
+            System.out.println(x);
         }
     }
 }
-
-/*
- * red
- * blue
- * orange
- * black
- * white
- * yellow
- * silver
- * green
- */
